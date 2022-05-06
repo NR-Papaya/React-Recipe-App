1819e9e99100cebcf44c95fc62df5e6b34517f9c
Nahia Riviera <n.riviera.dev@gmail.com>
2022-05-06 15:35:08 -0500

    Updates

---
+++ precache-manifest.0ea737d842c471345c8921c2ea41da14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cd0339346af4b87fa33742ff59738c",
+    "url": "/React-Recipe-App/index.html"
+  },
+  {
+    "revision": "fcfbdf47689015efa57f",
+    "url": "/React-Recipe-App/static/css/main.d9a2478c.chunk.css"
+  },
+  {
+    "revision": "f394498dd0083a989c2b",
+    "url": "/React-Recipe-App/static/js/2.6d741cec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Recipe-App/static/js/2.6d741cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcfbdf47689015efa57f",
+    "url": "/React-Recipe-App/static/js/main.371b8a0f.chunk.js"
+  },
+  {
+    "revision": "05ac211575e4d4bf7ea2",
+    "url": "/React-Recipe-App/static/js/runtime-main.65730133.js"
+  }
+]);
